1d522fea445b864c0a08ca092d83cf1cd794c553
Victor <victorhugopicerni@gmail.com>
2020-03-17 13:12:50 -0300

    Adicionei os comentários no programa

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 const int tam_buffer=100;
 
+/*funcao para inverter as palavras a partir de um espaco*/
 void inverte(char *buffer, int *k, int *j){
 	char tmp;
         tmp = buffer[*j];
@@ -18,14 +19,19 @@ void inverte(char *buffer, int *k, int *j){
 	
 }
 
+/*funcao para encontrar, separar e inverter as palavras*/
 void palavra(char *buffer){
+	if (buffer[tam] == '\n') buffer[tam] = '\0';
+	/*substituicao da quebra de linha por um fim de linha*/
+	
 	int k=0, j=0;
 	int tam = strlen(buffer) - 1;
- 	if (buffer[tam] == '\n') buffer[tam] = '\0';
+	/*contagem de caracteres*/ 	
 	int i;
 	for (i=0; i < tam ;i++){
 		
 		if((!isalnum(buffer[i])) || (i == tam - 1))
+		/*checagem de letras ou numeros, caso nao tenha uma letra ou um numero,o if funcionará*/		
 		{
 		if(i < tam - 1)	{
 			k = i - 1;
@@ -35,6 +41,7 @@ void palavra(char *buffer){
 			}
 
 		while (j<k)
+		/*troca de letras de acordo com o indice*/
 			{
 			inverte(buffer, &k, &j);
 			j++;
